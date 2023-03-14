c2afc4568021fb22359ca1d1f2bcd2d073aa3427
RPDH1 <127876686+RPDH1@users.noreply.github.com>
2023-03-14 17:23:35 +0000

    Add New Feature
    If the user types !ping the bot will reply "pong"
    
    You can use this basic feature to check the bot is working correctly.

--- README.md
+++ README.md
@@ -3,6 +3,7 @@
 
 ## Features
 * Sends a message to newly joined users on the Discord channel.
+* Added a test commant !ping. (Type this to see if the bot is working)
 
 ### Please Note
 Add your discord bot token to the bottom of the bot.py file
--- bot.py
+++ bot.py
@@ -3,9 +3,20 @@ import discord
 client = discord.Client()
 
 @client.event
+async def on_ready():
+    print(f"Logged in as {client.user}")
+
+@client.event
 async def on_member_join(member):
-    # This function is called whenever a new member joins the server
     message = f"Welcome to {member.guild.name}, {member.name}! We're happy you've joined us."
     await member.send(message)
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.startswith("!ping"):
+        await message.channel.send("pong")
 
 client.run("YOUR_BOT_TOKEN_HERE")
